834118508a7eadd69ac4eae65e79ea3b25154abb
ttoper <tyler.toper@gmail.com>
2021-07-30 17:52:52 -0700

    Comments

--- Start/Overview/Comments/Program.cs
+++ Start/Overview/Comments/Program.cs
@@ -6,10 +6,24 @@ namespace Comments
     {
         /// XML Comments are used to help provide documentation
         /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main function
+        /// </summary>
+        /// <param name='args'> An Array of string arguments from the command line </param>
+        /// <returns>
+        /// No returns
+        /// </returns>
+
         static void Main(string[] args)
         {
-
+            // Single Line Comment
+            // Can have many of these together
             Console.WriteLine("Hello World!");
+            /* This is a multiline comment
+
+                Still a comment
+
+            Now the comment is closed*/
 
         }
     }
