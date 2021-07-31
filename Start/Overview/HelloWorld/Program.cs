e333f0e26abf56467d5ddb1995264ffb7162655e
ttoper <tyler.toper@gmail.com>
2021-07-30 17:31:17 -0700

    Hello World Code

---
+++ Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+string name = "";
+
+Console.WriteLine("Hello World!");
+Console.WriteLine("What is your name?");
+name = Console.ReadLine();
+Console.WriteLine("Why, hello there " + name);
